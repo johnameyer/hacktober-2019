d509b14fd9885ab0ef639ab427af341b6f951ba8
John (Jack) Meyer <jack10042@gmail.com>
2019-10-06 14:36:48 -0400

    Hacktober Day 6

---
+++ 6/make.rs
@@ -0,0 +1,165 @@
+use std::env;
+use std::io::{Error, ErrorKind};
+use std::fs::{metadata, File};
+use std::path::Path;
+use std::io::{prelude::*, BufReader};
+use std::collections::{HashMap, HashSet};
+use std::process::exit;
+use std::time::SystemTime;
+use std::process::Command;
+
+struct Task {
+    name: String,
+    dependencies: HashSet<String>,
+    steps: Vec<String>
+}
+
+impl Task {
+    fn add_task(&mut self, task: String) {
+        self.steps.push(task);
+    }
+}
+
+fn main() -> std::io::Result<()> {
+    let args: Vec<String> = env::args().collect();
+    let mut tasks = None;
+    if args.len() > 1 {
+        tasks = Some(&args[1..]);
+    }
+
+    let path = Path::new("Makefile");
+    let display = path.display();
+
+    let file = match File::open(&path) {
+        Err(why) => panic!("couldn't open {}: {}", display,
+                                                   why),
+        Ok(file) => file,
+    };
+
+    let reader = BufReader::new(file);
+
+    let mut first_task = None;
+
+    let mut declared_tasks: HashMap<String, Task> = HashMap::new();
+
+    let mut last_task: Option<Task> = None;
+
+    for line in reader.lines() {
+
+        let unwrap = line.unwrap();
+
+        if unwrap.len() == 0 {
+            continue;
+        }
+
+        let ch = unwrap.chars().next().unwrap();
+        match ch {
+            '\t' => {
+                let command = unwrap.to_string();
+                if last_task.is_some() {
+                    last_task.as_mut().unwrap().add_task(command);
+                }
+            }
+            _ => {
+                let split: Vec<&str> = unwrap.splitn(2, ':').collect();
+                let task = split[0];
+                
+                let dependencies: Vec<&str> = split[1].split_whitespace().collect();
+
+                if last_task.is_some() {
+                    let x = last_task.unwrap();
+                    declared_tasks.insert(x.name.to_string(), x);    
+                }
+
+                last_task = Some(Task {
+                    name: task.to_string(),
+                    dependencies: dependencies.iter().map(|x| x.to_string()).collect(),
+                    steps: Vec::new()
+                });
+
+                if first_task.is_none() {
+                    first_task = Some(task.to_string());
+                }
+            }
+        }
+    }
+
+    if let Some(last) = last_task {
+        declared_tasks.insert(last.name.to_string(), last);
+    }
+
+    if let Some(unwrapped_tasks) = tasks {
+        for task in unwrapped_tasks {
+            let mut done: HashSet<String> = HashSet::new();
+            run(&task, &declared_tasks, &mut done);
+        }
+    } else {
+        if let Some(default) = first_task {
+            let mut done: HashSet<String> = HashSet::new();
+            run(&default, &declared_tasks, &mut done);
+        } else {
+            return Err(Error::new(ErrorKind::Other, "No tasks defined in file"));
+        }
+    }
+    return Ok(());
+}
+
+fn run_step(step: &String) {
+    println!("{}", step);
+}
+
+fn run(task: &String, declared_tasks: &HashMap<String, Task>, done: &mut HashSet<String>) -> std::io::Result<()> {
+    if done.contains(task) {
+        return Ok(());
+    }
+
+    let found = declared_tasks.get(task);
+    if found.is_some() {
+        done.insert(task.to_string());
+        let found_task = found.as_ref().unwrap();
+        for dependency in found_task.dependencies.iter() {
+            run(&dependency, declared_tasks, done);
+        }
+        if should_run(found_task, declared_tasks) {
+            println!("{}", task);
+            for step in found_task.steps.iter() {
+                run_step(step);
+            }
+        }
+    } else {
+        return Err(Error::new(ErrorKind::Other, format!("Task {} not declared.\n", task)));
+    }
+    return Ok(());
+}
+
+fn should_run(task: &Task, declared_tasks: &HashMap<String, Task>) -> bool {
+    let task_updated = get_updated(&task.name);
+
+    if task.dependencies.len() == 0 {
+        return true;
+    }
+
+    for dependency in task.dependencies.iter() {
+        let dependency_updated = get_updated(dependency);
+
+        if task_updated.is_ok() && dependency_updated.is_ok() {
+            if task_updated.as_ref().unwrap() < dependency_updated.as_ref().unwrap() {
+                return true;
+            }
+        } else {
+            return true;
+        }
+    }
+
+    return false;
+}
+
+fn get_updated(task: &String) -> std::io::Result<SystemTime> {
+    let data = metadata(task)?;
+
+    if let Ok(time) = data.modified() {
+        Ok(time)
+    } else {
+        Err(Error::new(ErrorKind::Other, "Not supported on this platform"))
+    }
+}
